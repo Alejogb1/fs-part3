dc156921254c80b33ed7ca49f96dcef5e7f01ca5
alejogb1 <devsalejo@gmail.com>
2021-11-05 12:45:26 -0300

    exercises 3.17 3.18 3.19

--- index.js
+++ index.js
@@ -27,18 +27,31 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+app.use(express.static("build"));
 
 const mongoose = require("mongoose");
+const { response } = require("express");
 
 const url = `mongodb+srv://alejogb:${process.argv[2]}@cluster0.yvdek.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
   number: Number,
 });
 
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 const phoneNumber = mongoose.model("Note", noteSchema);
 
 if (process.argv.length > 3) {
@@ -47,7 +60,6 @@ if (process.argv.length > 3) {
     number: process.argv[4],
   });
   number.save().then((result) => {
-    console.log("RESULT, ", result);
     console.log(
       `Added number: ${result.number} of ${result.name} to phonebook`
     );
@@ -57,17 +69,12 @@ if (process.argv.length > 3) {
   console.log("Ejecutar find");
 
   phoneNumber.find({}).then((result) => {
-    console.log("phonebook: ");
     result.forEach((note) => {
       console.log(note.name, " ", note.number);
     });
-    mongoose.connection.close();
   });
 }
 
-app.use(express.static("build"));
-
-app.get("/", (req, res) => {});
 app.use(
   morgan((tokens, req, res) => {
     return [
@@ -83,21 +90,32 @@ app.use(
   })
 );
 app.get("/api/persons", (request, response) => {
-  response.send(persons);
+  phoneNumber
+    .find({})
+    .then((persons) => {
+      if (persons) {
+        response.json(persons);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: "malformatted id" });
+    });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  try {
-    const id = request.params.id;
-    console.log(id);
-    let person = persons.find((person) => {
-      return toString(person.id) === toString(id);
-    });
-    console.log("PERSON", person);
-    response.json(person);
-  } catch (error) {
-    console.log("ERRROR", error);
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  phoneNumber
+    .findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person.toJSON());
+      } else {
+        response.json(person.toJSON());
+      }
+    })
+    .catch((error) => next(error));
 });
 app.delete("/api/persons/:id", (request, response) => {
   try {
@@ -111,17 +129,32 @@ app.delete("/api/persons/:id", (request, response) => {
     console.log("ERROR", error);
   }
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
 app.use(
   express.json({
     type: ["application/json", "text/plain"],
   })
 );
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
+  console.log("NAME: ", body.number);
   const personName = body.name;
   const personNumber = body.number;
-
+  if (body === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
   function hasDuplicates(newElement) {
     let object = persons.find((element) => element.name === newElement);
     console.log("OBJECT ", object);
@@ -138,13 +171,33 @@ app.post("/api/persons", (request, response) => {
   } else if (hasDuplicates(personName)) {
     response.json({ error: "name is alreaded added" });
   }
-  return response.send(JSON.stringify(body));
+  const number = new phoneNumber({
+    name: personName,
+    number: personNumber,
+  });
+
+  number
+    .save()
+    .then((savedNumber) => {
+      response.json(savedNumber.toJSON());
+    })
+    .catch((error) => next(error));
 });
 app.get("/info", (request, response) => {
   response.send(
     `Phonebook has info for ${persons.length} people ${new Date()}`
   );
 });
+app.put("/api/persons", (request, response) => {
+  let updatedNumbers = phoneNumber.findOneAndUpdate(request.param.object, {
+    name: request.params.name,
+  });
 
-app.listen(process.env.PORT || 5000);
+  console.log("OBJECT PUT: ", request.param.object);
+
+  response.send(updatedNumbers);
+});
+app.use(errorHandler);
+
+app.listen(process.env.PORT || 3001);
 console.log(`Server running on port ${5000}`);
